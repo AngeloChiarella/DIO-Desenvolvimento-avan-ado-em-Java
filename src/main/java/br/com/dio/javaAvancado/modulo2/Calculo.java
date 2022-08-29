2ff57da35ed86f0fc070004ccd91db6c70bbd4ef
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-08-29 15:55:34 -0300

    Funções de alta ordem

--- src/main/java/br/com/dio/javaAvancado/modulo2/Calculo.java
+++ src/main/java/br/com/dio/javaAvancado/modulo2/Calculo.java
@@ -1,5 +1,6 @@
 package br.com.dio.javaAvancado.modulo2;
 
+@FunctionalInterface
 public interface Calculo {
 	public int somar(int a, int b);
 }
---
+++ src/main/java/br/com/dio/javaAvancado/modulo2/Funcoes.java
@@ -0,0 +1,12 @@
+package br.com.dio.javaAvancado.modulo2;
+
+import java.util.function.Function;
+
+public class Funcoes {
+
+	public static void main(String[] args) {
+		Function<String, String> retornarNomeAoContrario = texto -> new StringBuilder(texto).reverse().toString();
+		System.out.println(retornarNomeAoContrario.apply("Angelo"));
+	}
+
+}
